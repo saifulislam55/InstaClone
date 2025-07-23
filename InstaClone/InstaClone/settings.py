56678160831d6edf123a52ddfdb217153c6affdf
Saif Islam~ <sumairamubeen7@gmail.com~>
2025-07-24 00:37:47 +0500

    'saif'

--- InstaClone/InstaClone/settings.py
+++ InstaClone/InstaClone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+$n0r+!)cg$izmm8qdzqjli+$gavtw_iy^iw)lpit_*zks$qo!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,7 +127,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+import os
+
+# Add this at the top of settings.py
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Required for Whitenoise
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
